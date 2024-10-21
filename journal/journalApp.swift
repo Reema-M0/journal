0e1ffe5797818462ed3f9d02095a48b874bb31de
Reema-M0 <Reemaal200200@gmil.com>
2024-10-21 10:14:27 +0300

    Initial Commit

---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Reem on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
