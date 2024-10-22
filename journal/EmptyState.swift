863968002969197d3b09fc7da8bda980c60461ba
Reema-M0 <Reemaal200200@gmil.com>
2024-10-22 09:45:58 +0300

    h

---
+++ journal/EmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyState.swift
+//  journal
+//
+//  Created by Reem on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyState()
+}
